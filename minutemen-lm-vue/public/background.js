ea9a265b38266e240ee713349c4c92e4cc667633
Sören <5426009+soerentsch@users.noreply.github.com>
2025-03-26 15:16:55 +0100

    remove poc

--- minutemen-lm-vue/public/background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(function(tab) {
-    chrome.windows.create({
-      url: 'index.html',
-      type: 'popup',
-      width: 800,
-      height: 600
-    });
-  });
--- minutemen-lm-vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import BookmarkList from './components/BookmarkList.vue'
-</script>
-
-<template>
-  <div>
-    <BookmarkList />
-  </div>
-</template>
